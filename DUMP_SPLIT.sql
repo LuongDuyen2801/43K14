f3f7c6fbea782c0e465a8e6ab3e3254127a3c87b
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-01-13 11:34:20 +0700

    Update DUMP_SPLIT.sql

--- DUMP_SPLIT.sql
+++ DUMP_SPLIT.sql
@@ -5,3 +5,4 @@ select right (ten, CHARINDEX(' ',reverse(ten)) - 1) from fckdump --cắt tên
 
 select left(ten, len(ten) - CHARINDEX(' ',reverse(ten))) ten1 from fckdump -- cắt họ và tên đệm
 
+Mạnh đã vào đây sửa
