9785a3c78e4b89ef9ce11b674acc2304134ff430
LuongDuyen2801 <59547357+LuongDuyen2801@users.noreply.github.com>
2020-01-13 11:42:50 +0700

    Update DUMP_SPLIT.sql

--- DUMP_SPLIT.sql
+++ DUMP_SPLIT.sql
@@ -6,13 +6,7 @@ select right (ten, CHARINDEX(' ',reverse(ten)) - 1) from fckdump --cắt tên
 
 select left(ten, len(ten) - CHARINDEX(' ',reverse(ten))) ten1 from fckdump -- cắt họ và tên đệm
 
-<<<<<<< HEAD
-Mạnh đã vào đây sửa
-=======
 --Việt
 --Đây là 1 comment
-<<<<<<< HEAD
->>>>>>> b40e7711b786c2e5a41a9c9bd5c06dbf83a20a4b
-=======
 --Nhi seen
->>>>>>> 67f76f07973eeac6e704feee233d6ea037103a75
+--nhkjkjjkjllkl
